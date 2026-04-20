5c830f3f006f96c19103f95b9d03ca4812996238
SuruhBeli <sipittrader@gmail.com>
2026-04-21 06:28:59 +0700

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyByQl0BXZoSMzrULUNA6l7UVFQjXmvsdJE",
+  authDomain: "suruhbeli-e8ae8.firebaseapp.com",
+  projectId: "suruhbeli-e8ae8",
+  messagingSenderId: "5783247867",
+  appId: "1:5783247867:web:8f57e09a7dc4565378c95e"
+});
+
+const messaging = firebase.messaging();
